c552178ca9616bda346d9c465ba482a577491982
shaikh aaref hamza <skaarefz@gmail.com>
2025-02-03 21:28:15 +0530

    custom-react-v2

--- custum-React/custom-main.js
+++ custum-React/custom-main.js
@@ -2,13 +2,23 @@
 
 
 function customrender(reactElement,container){
-   const domElement=document.createElement(reactElement.type)
-   domElement.setAttribute('href',reactElement.props.href)
-   domElement.setAttribute('target',reactElement.props.target)
-   domElement.innerHTML=reactElement.value
-   container.appendChild(domElement)
-   
+//    const domElement=document.createElement(reactElement.type)
+//    domElement.setAttribute('href',reactElement.props.href)
+//    domElement.setAttribute('target',reactElement.props.target)
+//    domElement.innerHTML=reactElement.value
+//    container.appendChild(domElement)
+
+const domElement=document.createElement(reactElement.type)
+domElement.innerHTML=reactElement.value
+for (const prop in reactElement.props) {
+    if(prop==reactElement.value) continue;
+    domElement.setAttribute(prop,reactElement.props[prop])
+    
+    }
+    container.appendChild(domElement)
 }
+   
+
 
 
 const reactElement={
