8f8bb32d589d8b44703090f6dedaa5bd2db59d5e
shaikh aaref hamza <skaarefz@gmail.com>
2025-02-05 14:15:18 +0530

    add card and props in react

--- 03project/src/App.jsx
+++ 03project/src/App.jsx
@@ -2,26 +2,23 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './componants/card'
 
 function App() {
-  
-
+    
+// let obj={
+//   name:'shaikh',
+//   age:21
+// }
+// let nearry=[1,2,3,4]
   return (
     <>
-      <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
-  <div>
-    <img className="size-48 shadow-xl rounded-md" alt="" src="\src\assets\download.jpeg" />
-  </div>
-  <div className="flex items-center md:items-start">
-    <span className="text-2xl font-medium">Class Warfare</span>
-    <span className="font-medium text-sky-500">The Anti-Patterns</span>
-    <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-      <span>No. 4</span>
-      <span>·</span>
-      <span>2025</span>
-    </span>
-  </div>
-</div>
+
+    <h1 className='bg-green-500 rounded-2xl mb-5 font-semibold '>tailwind test</h1>
+          
+        {/* { <Card newobj={nearry}/> } */}
+        <Card username='Scince' title='SCINECE'  />
+        <Card username='BIO' title='BIOLOGY' />
     </>
   )
 }
