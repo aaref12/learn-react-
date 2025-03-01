bfe4ea5003ca6b6377ec1c34a3fb4d3bbc43c14c
shaikh aaref hamza <skaarefz@gmail.com>
2025-03-01 13:26:28 +0530

    mega project add

---
+++ 14MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import config from './config/configEnv'
+
+function App() {
+ console.log(config.Bucket)
+  return (
+    <>
+     <h1>react app with apwrite </h1> 
+    </>
+  )
+}
+
+export default App
